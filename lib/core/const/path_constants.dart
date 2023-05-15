75ee78e9546ed35230ec5e444aae6af1a2b47ffe
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-05-16 02:18:03 +0300

    Add onboarding assets

---
+++ lib/core/const/path_constants.dart
@@ -0,0 +1,6 @@
+class PathConstants {
+  // Onboarding
+  static const String onboarding1 = 'assets/images/onboarding/onboarding.jpg';
+  static const String onboarding2 = 'assets/images/onboarding/onboarding_2.jpg';
+  static const String onboarding3 = 'assets/images/onboarding/onboarding_3.jpg';
+}
