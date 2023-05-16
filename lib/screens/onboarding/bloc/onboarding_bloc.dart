ea23d8eb99270e31ac169c5dea44713b3b3bb232
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-05-17 01:28:26 +0300

    Fix swipping events on onboarding screen

--- lib/screens/onboarding/bloc/onboarding_bloc.dart
+++ lib/screens/onboarding/bloc/onboarding_bloc.dart
@@ -5,31 +5,30 @@ part 'onboarding_event.dart';
 part 'onboarding_state.dart';
 
 class OnboardingBloc extends Bloc<OnboardingEvent, OnboardingState> {
-  OnboardingBloc() : super(OnboardingInitial());
   int pageIndex = 0;
   final pageController = PageController(initialPage: 0);
 
-  @override
-  Stream<OnboardingState> mapEventToState(
-    OnboardingEvent event,
-  ) async* {
-    if (event is PageChangedEvent) {
+  OnboardingBloc() : super(OnboardingInitial()) {
+    on<PageChangedEvent>((event, emit) {
       if (pageIndex == 2) {
-        yield NextScreenState();
+        emit(NextScreenState());
         return;
       }
+
       pageIndex += 1;
 
       pageController.animateToPage(
         pageIndex,
-        duration: Duration(milliseconds: 500),
+        duration: const Duration(milliseconds: 500),
         curve: Curves.ease,
       );
 
-      yield PageChangedState(counter: pageIndex);
-    } else if (event is PageSwipedEvent) {
+      emit(PageChangedState(counter: pageIndex));
+    });
+
+    on<PageSwipedEvent>((event, emit) {
       pageIndex = event.index;
-      yield PageChangedState(counter: pageIndex);
-    }
+      emit(PageChangedState(counter: pageIndex));
+    });
   }
 }
