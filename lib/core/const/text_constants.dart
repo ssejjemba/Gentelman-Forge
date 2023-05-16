beb3a42e9a5e5dc26dd8567d26fcfc35e0e7531f
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-05-17 01:35:03 +0300

    Add text that is specific to the application

--- lib/core/const/text_constants.dart
+++ lib/core/const/text_constants.dart
@@ -1,12 +1,12 @@
 class TextConstants {
   // Onboarding
-  static const String onboarding1Title = "Workout anywhere";
-  static const String onboarding2Title = "Learn techniques";
-  static const String onboarding3Title = "Stay strong & healthy";
+  static const String onboarding1Title = "Welcome, Style Pioneer!";
+  static const String onboarding2Title = "Discover Your Unique Style";
+  static const String onboarding3Title = "Tailored Just for You";
   static const String onboarding1Description =
-      "You can do your workout at home without any equipment, outside or at the gym.";
+      "Begin your sartorial journey with Gentleman's Forge, where we redefine men's fashion. Explore, express, and elevate your style with us.";
   static const String onboarding2Description =
-      "Our workout programs are made by professionals.";
+      "Navigate through our diverse collections, tailor-made for the modern man. Find pieces that resonate with your personality and amplify your style quotient.";
   static const String onboarding3Description =
-      "We want you to fully enjoy the program and stay healthy and positive.";
+      "With personalized recommendations and seamless shopping, we cater to your individual style. Forge your distinct fashion identity with Gentleman's Forge.";
 }
