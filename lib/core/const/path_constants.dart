53406566df50e0cd7b4d3dc305fb81d2d53526ce
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-05-17 02:25:12 +0300

    Update path constants

--- lib/core/const/path_constants.dart
+++ lib/core/const/path_constants.dart
@@ -3,4 +3,6 @@ class PathConstants {
   static const String onboarding1 = 'assets/images/onboarding/onboarding.jpg';
   static const String onboarding2 = 'assets/images/onboarding/onboarding_2.jpg';
   static const String onboarding3 = 'assets/images/onboarding/onboarding_3.jpg';
+  // Auth
+  static const String eye = 'assets/images/auth/eye_icon.png';
 }
