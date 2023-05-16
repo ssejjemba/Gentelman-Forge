ffd040cc4973b48acdf46c6bf6a046d8e57ff5f8
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-05-17 02:26:11 +0300

    Update app text constants

--- lib/core/const/text_constants.dart
+++ lib/core/const/text_constants.dart
@@ -9,4 +9,29 @@ class TextConstants {
       "Navigate through our diverse collections, tailor-made for the modern man. Find pieces that resonate with your personality and amplify your style quotient.";
   static const String onboarding3Description =
       "With personalized recommendations and seamless shopping, we cater to your individual style. Forge your distinct fashion identity with Gentleman's Forge.";
+
+  // Sign Up
+  static const String signUp = "Sign up";
+  static const String signIn = "Sign In";
+  static const String signOut = "Sign Out";
+
+  static const String username = "Username";
+  static const String userNamePlaceholder = "Your name";
+  static const String usernameErrorText = 'Text is required';
+
+  static const String email = "Email";
+  static const String emailPlaceholder = "example@mail.com";
+  static const String emailErrorText =
+      'Email is unvalid, please enter email properly';
+
+  static const String password = "Password";
+  static const String passwordPlaceholder = "Must be at least 6 symbols";
+  static const String passwordErrorText =
+      'Password should contain at least 6 characters';
+
+  static const String confirmPassword = "Confirm password";
+  static const String confirmPasswordPlaceholder = "Re-enter password";
+  static const String confirmPasswordErrorText = 'Password is not the same';
+
+  static const String alreadyHaveAccount = "Already have an account?";
 }
